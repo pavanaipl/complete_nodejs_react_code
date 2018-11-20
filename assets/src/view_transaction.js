cb3aebb94b2d468f68a94c8b986a46e050f53803
pavannagaraj <pavanmn8@gmail.com>
2018-11-20 09:58:45 +0530

    changed the table style

--- assets/src/view_transaction.js
+++ assets/src/view_transaction.js
@@ -41,21 +41,42 @@ export default class ViewTrasactionDetails extends Component {
    
     return (
     <div className="container-fluid custom_container" style={{height:100}}>
-      <div className="col-md-6">
+      <div className="col-md-12">
 
          <table style={{marginTop:50}} className="table table-hover table-striped">
+           <thead>
+                    <tr>
+                          <th>ContactId</th>
+                          <th >Contract Status</th>
+                          <th >Portfolio</th>
+                          <th >Portfolio Group</th>
+                          <th >Counterparty</th>
+                          <th >SecurityType</th>
+                          <th >SecurityGroup</th>
+                          <th >Dealer</th>
+                          <th >TradeDate</th>
+                          <th >Settlement Days</th>
+                          <th >Settlement Date</th>
+                          
+                    </tr>
+            </thead>
+
+
+
             <tbody>
-             <tr><td style ={{marginRight:30}}> ContractId : {this.props.location.state.id.contract_id} </td></tr>
-             <tr><td>ContractStatus : {this.props.location.state.id.contract_status} </td></tr>
-             <tr><td> Portfolio : {this.props.location.state.id.portfolio} </td> </tr>
-             <tr><td> PortfolioGroup : {this.props.location.state.id.portfolio_group}</td></tr>
-             <tr><td> Counterparty : {this.props.location.state.id.counterparty}</td></tr>
-             <tr><td> SecurityType : {this.props.location.state.id.security_type}</td></tr>
-             <tr><td> SecurityGroup : {this.props.location.state.id.security_group}</td></tr>
-             <tr><td> Dealer : {this.props.location.state.id.dealer}</td></tr>
-             <tr><td> TradeDate : {this.props.location.state.id.trade_date}</td></tr>
-             <tr><td>  SettlementDays : {this.props.location.state.id.settlement_days}</td></tr>
-             <tr><td> SettlementDate : {this.props.location.state.id.settlement_date}</td></tr>
+             <tr> 
+                 <td>{this.props.location.state.id.contract_id} </td>
+                 <td>{this.props.location.state.id.contract_status} </td>
+                 <td>{this.props.location.state.id.portfolio} </td> 
+                 <td>{this.props.location.state.id.portfolio_group}</td>
+                 <td>{this.props.location.state.id.counterparty}</td>
+                 <td>{this.props.location.state.id.security_type}</td>
+                 <td>{this.props.location.state.id.security_group}</td>
+                 <td>{this.props.location.state.id.dealer}</td>
+                 <td>{this.props.location.state.id.trade_date}</td>
+                 <td>{this.props.location.state.id.settlement_days}</td>
+                 <td>{this.props.location.state.id.settlement_date}</td>
+             </tr>
             </tbody>
          </table>
       </div>
